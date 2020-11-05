218c0fbe15e89a29daa3ff59c6fcae7eec87e544
leon matias <roslin.leon.cix@outlook.com>
2020-11-04 23:19:17 -0500

    add project

---
+++ src/main/java/com/rleon/webflux/miscelaneos/springwebfluxp02miscelaneos/SpringWebfluxP02MiscelaneosApplication.java
@@ -0,0 +1,13 @@
+package com.rleon.webflux.miscelaneos.springwebfluxp02miscelaneos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxP02MiscelaneosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxP02MiscelaneosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rleon/webflux/miscelaneos/springwebfluxp02miscelaneos/SpringWebfluxP02MiscelaneosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rleon.webflux.miscelaneos.springwebfluxp02miscelaneos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxP02MiscelaneosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
